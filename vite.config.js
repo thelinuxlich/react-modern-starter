ba402f1044472a5c019b69d0992bb961a6f6d2d1
Alisson <thelinuxlich@gmail.com>
2021-03-08 22:26:06 -0300

    feat: added valtio/macro

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import WindiCSS from 'vite-plugin-windicss'
-
+import babel from 'vite-babel-plugin'
 /**
  * https://vitejs.dev/config/
  * @type { import('vite').UserConfig }
  */
 export default {
-  plugins: [WindiCSS(), reactRefresh()]
+  plugins: [babel(), WindiCSS(), reactRefresh()]
 }
